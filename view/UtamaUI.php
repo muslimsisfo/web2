4c3c1c5a197249eb103e137158c535b87b858a28
muslim <muslimcavinhery@gmail.com>
2017-05-24 14:05:40 +0700

    menampilkan halaman depan dari database

--- view/UtamaUI.php
+++ view/UtamaUI.php
@@ -8,6 +8,9 @@
 {
 	public function tampilBerita()
 	{
+		include_once 'model/Berita.php';
+		$brt= new Berita();
+		$isi_berita= $brt->ambilberita();
 		include_once 'pages/beranda.php';
 		$this->end();
 	}
